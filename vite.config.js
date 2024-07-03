a418099c6561122bfd46d581165aab213d6850d1
BHASKAR VVSG <bhaskarvvsg57hds@gmail.com>
2024-07-04 00:21:12 +0530

    updated the code with port 5173

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173,
+  },
+});
