2e1e7b658c757faef853dd8710e706ce24657b97
BHASKAR VVSG <bhaskarvvsg57hds@gmail.com>
2024-07-04 00:04:00 +0530

    changed to the old code by removing the port  number

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 5173,
-  },
-});
+})
