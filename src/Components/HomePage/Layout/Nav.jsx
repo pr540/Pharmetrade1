5cdb363c504de7e743cd9cdd3a0e04eb5fb79995
nabi06 <125720183+nabi06@users.noreply.github.com>
2024-07-15 10:04:15 +0530

    Recent Commit

--- src/Components/HomePage/Layout/Nav.jsx
+++ src/Components/HomePage/Layout/Nav.jsx
@@ -30,6 +30,18 @@ import { IoLogoInstagram } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
 // import { useNavbarContext } from "./NavbarContext";
 import myaccount from "../../../assets/My Account.png";
+import { TbTruckReturn } from "react-icons/tb";
+import Baby from "../../All Category/Baby";
+import Beauty from "../../All Category/Beauty";
+import Grocery from "../../All Category/Grocery";
+import HealthTopics from "../../All Category/HealthTopics";
+import Herbs from "../../All Category/Herbs";
+import Home from "../../All Category/Home";
+import Medicines from "../../All Category/Medicines";
+import PersonalCare from "../../All Category/PersonalCare";
+import Pets from "../../All Category/Pets";
+import SportsNutrition from "../../All Category/SportsNutrition";
+import Suppliments from "../../All Category/Suppliments";
 
 function Nav({ topDivRef, cartItems }) {
   const MenuItems = [
@@ -50,6 +62,24 @@ function Nav({ topDivRef, cartItems }) {
     { label: "Insta", icon: insta, path: "#" },
     { label: "Twitter", icon: twitter, path: "#" },
   ];
+  const components = [
+    { name: "Allergies", component: <Baby /> },
+    { name: "Bath & Body ", component: <Beauty /> },
+    { name: "Coough Cold & flu", component: <HealthTopics /> },
+    { name: "Digestive", component: <Home /> },
+    { name: "drug", component: <Medicines /> },
+    { name: "Eye Care", component: <PersonalCare /> },
+    { name: "Ear Nose", component: <Pets /> },
+    { name: "First Aid", component: <SportsNutrition /> },
+    { name: "Foot Care", component: <Suppliments /> },
+    { name: "Home  Test Monitoring ", component: <Suppliments /> },
+    { name: "Incontinence ", component: <Suppliments /> },
+    { name: "Pain Relief & Management ", component: <Suppliments /> },
+    { name: "Pediatric Care ", component: <Suppliments /> },
+    { name: "Personal Care ", component: <Suppliments /> },
+    { name: "Pet  ", component: <Suppliments /> },
+    { name: "Prescription & Packaging  ", component: <Suppliments /> },
+  ];
 
   const handleSelect = (index) => {
     if (MenuItems[index] == "Home") navigate("/app");
@@ -58,8 +88,9 @@ function Nav({ topDivRef, cartItems }) {
 
   let navigate = useNavigate();
 
-
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [isCategory, setIsCategory] = useState(false);
+  const [popUps, setPopUps] = useState(<Baby />);
 
   const handleMouseEnter = () => {
     setIsPopupVisible(true);
@@ -68,7 +99,12 @@ function Nav({ topDivRef, cartItems }) {
   const handleMouseLeave = () => {
     setIsPopupVisible(false);
   };
-
+  const mouseUp = () => {
+    setIsCategory(true);
+  };
+  const mouseDown = () => {
+    setIsCategory(false);
+  };
   function handleredirect() {
     navigate("/login");
   }
@@ -81,13 +117,16 @@ function Nav({ topDivRef, cartItems }) {
     navigate("/wishlist");
   }
 
-  
   function handleuser() {
     navigate("/user");
   }
   function handleorder() {
     navigate("/orderhistory");
   }
+  function hanldeUp(items) {
+    setPopUps(items);
+  }
+
   return (
     <div
       ref={topDivRef}
@@ -142,12 +181,12 @@ function Nav({ topDivRef, cartItems }) {
                     <div className="fixed flex z-10">
                       <div className="bg-white p-4 rounded shadow-lg w-64">
                         <div className="w-full justify-center items-center">
-                          <button
-                            className="bg-blue-900 text-white py-1 px-2 rounded mb-2"
+                          <a
+                            className="bg-blue-900 text-white py-1 px-2 rounded "
                             onClick={handleredirect}
                           >
                             Sign In
-                          </button>
+                          </a>
                         </div>
                         <p className="mb-1 text-lg">New customer? Start here</p>
                         <h2
@@ -157,7 +196,7 @@ function Nav({ topDivRef, cartItems }) {
                           Your Account
                         </h2>
                         <ul>
-                          <li className="mb-2">
+                          <li className="">
                             <a
                               href="#"
                               className="text-lg text-blue-900"
@@ -166,7 +205,7 @@ function Nav({ topDivRef, cartItems }) {
                               Order List
                             </a>
                           </li>
-                          <li className="mb-2">
+                          <li className="">
                             <a
                               href="#"
                               className="text-blue-900"
@@ -175,7 +214,7 @@ function Nav({ topDivRef, cartItems }) {
                               Wishlist
                             </a>
                           </li>
-                          <li className="mb-2">
+                          <li className="">
                             <Link to="/admin" className="text-lg text-blue-900">
                               Seller Dashboard
                             </Link>
@@ -192,7 +231,7 @@ function Nav({ topDivRef, cartItems }) {
                 </div>
 
                 <li className="relative ">
-                  <button>
+                  <a>
                     {/* <img
                       src={Cart}
                       className="w-6 md:w-8 lg:w-10 xl:w-12 pt-2 h-10 md:h-10 lg:h-10 xl:h-12"
@@ -203,7 +242,7 @@ function Nav({ topDivRef, cartItems }) {
                       className="w-2  md:w-4 lg:w-6 xl:w-8 pt-2 h-2 md:h-4 lg:h-6 xl:h-8 text-blue-900 hover:text-gray-400 hover:scale-110 transi duration-500"
                       onClick={handleCart}
                     />
-                  </button>
+                  </a>
                   <div
                     className={`absolute -top-2  text-blue-900 right-4 font-semibold ${
                       cartItems.length == 0 ? "hidden" : ""
@@ -213,22 +252,22 @@ function Nav({ topDivRef, cartItems }) {
                   </div>
                 </li>
                 <li>
-                  <button>
+                  <a>
                     <img
                       src={like}
                       onClick={handleclick}
                       className="w-2 md:w-4 lg:w-6 xl:w-8 pt-2 h-2 md:h-4 lg:h-6 xl:h-8 hover:scale-110 transition duration-300"
                     />{" "}
-                  </button>
+                  </a>
                 </li>
                 <li>
-                  <button>
+                  <a>
                     <img
                       src={compare}
                       onClick={handleclick}
                       className="w-2 md:w-4 lg:w-6 xl:w-8 pt-2 h-2 md:h-4 lg:h-6 xl:h-8 hover:scale-110 transition duration-300"
                     />{" "}
-                  </button>
+                  </a>
                 </li>
               </div>
             </div>
@@ -240,37 +279,61 @@ function Nav({ topDivRef, cartItems }) {
            items-center text-black border-2 border-grey-500 shadow-lg  "
         >
           <div className="flex items-center  w-[40%]  lg:gap-10 ">
-            <div
-              className="w-full  flex items-center"
-              // style={{ width: "100vh" }}
-            >
-              <select className="h-8 p-1   text-[12px] w-[80px] px-5 text-gray-500 bg-white border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-                <option>All categories</option>
-                <option>Allegies</option>
-                <option>Bath & Body</option>
-                <option>Cough Cold & flu</option>
-                <option>Digestive Health</option>
-                <option>Drug</option>
-                <option>Eye Care</option>
-                <option>Ear Nose & Throat Care</option>
-                <option>First Aid</option>
-                <option>Foot Care</option>
-                <option>Home Test Monitoring</option>
-                <option>Incontinence</option>
-                <option>Pain Relief & Management</option>
-                <option>Pediatric Care</option>
-                <option>Personal Care</option>
-                <option>Pet</option>
-                <option>Presciption & Packaging</option>
+            <div className="w-full relative flex items-center">
+              <a
+                className="h-8 px-3 pt-2 text-left text-[12px] text-gray-500 bg-white border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onMouseEnter={mouseUp}
+                onMouseLeave={mouseDown}
+              >
+                All Categories
+                {isCategory && (
+                  <div className="">
+                    <div
+                      className=" absolute z-10"
+                      style={{
+                        top: "30px",
+                        left: "0px",
+                      }}
+                    >
+                      <div className="bg-white px-4 py-3 rounded shadow-lg w-64">
+                        <div className="w-full justify-center items-center"></div>
+                        {components.map((items, index) => (
+                          <ul key={index}>
+                            <li className="mt-2">
+                              <a
+                                className="hover:text-black text-xl m text-blue-500"
+                                onClick={() => hanldeUp(items.name)}
+                                onMouseLeave={() => handleLeave(items.name)}
+                              >
+                                {items.name}
+                              </a>
+                              {popUps === items.name && (
+                                <div
+                                  className="absolute bg-white border border-gray-300 rounded shadow-lg"
+                                  style={{
+                                    top: "0%",
+                                    left: "100%",
+                                    width: "150px",
+                                  }} // Adjust width and position as needed
+                                >
+                                  {items.component}
+                                </div>
+                              )}
+                            </li>
+                          </ul>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </a>
 
-                {/* Add more options here */}
-              </select>
               <input
                 type="text"
                 placeholder="Search for products..."
-                className="w-full h-8 p-2 px-4 border border-gray-300 focus:outline-none  focus:ring-2 focus:ring-blue-500"
+                className="flex-grow h-8 p-2 px-4 border-t border-b border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <button className=" w-[40px] flex items-center justify-center h-8 p-2  bg-blue-500 text-white rounded-r-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+              <a className="w-[40px] flex items-center justify-center h-8 p-2 bg-blue-500 text-white border border-blue-500 rounded-r-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                 <svg
                   className="w-full h-full"
                   fill="none"
@@ -285,22 +348,25 @@ function Nav({ topDivRef, cartItems }) {
                     d="M21 21l-4.35-4.35m1.85-2.15A7 7 0 1113 5a7 7 0 015 12.35z"
                   />
                 </svg>
-              </button>
+              </a>
             </div>
           </div>
           <div className="flex gap-5  items-center justify-around text-blue-900 text-xs p-4 w-full md:w-fit">
             {downDivItems.map((item, index) => (
               <li
                 key={index}
-                onClick={()=>navigate(item.path)}
+                onClick={() => navigate(item.path)}
                 className="flex gap-1 items-center justify-center cursor-pointer hover:text-green-400"
               >
-                <img src={item.icon} className="max-w-8 max-h-8" alt={item.label} />
+                <img
+                  src={item.icon}
+                  className="max-w-8 max-h-8"
+                  alt={item.label}
+                />
                 <div className="text-[15px] ml-1 ">{item.label}</div>
               </li>
             ))}
           </div>
-          
         </div>
       </div>
     </div>
